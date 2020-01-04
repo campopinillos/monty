85b822f0010351164fde71810def82b91b349896
campopinillos <1207@holbertonschool.com>
2020-01-04 12:48:15 -0500

    [Fix] Code betty style

--- monty_aux.c
+++ monty_aux.c
@@ -6,13 +6,13 @@
  * Description: Determine if is it a number between 0 and 9
  * Return: 1 if it is a number 0 if is not
  */
-int _isdigit(char *str)
+int _isdigit(char *c)
 {
 	int i;
 
-	for (i = 0; str[i]; i++)
+	for (i = 0; c[i]; i++)
 	{
-		if (str[i] < 48 || str[i] > 57)
+		if (c[i] < 48 || c[i] > 57)
 			return (0);
 	}
 	return (1);
